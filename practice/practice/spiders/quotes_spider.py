a65489bdebaa42e777c3872885c5c0177f6a3d95
ArslanMansha <arslan.mansha.arbisoft.com>
2018-08-28 17:10:23 +0500

    Scrapping all pages

--- practice/practice/spiders/quotes_spider.py
+++ practice/practice/spiders/quotes_spider.py
@@ -5,7 +5,6 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
     ]
 
     def parse(self, response):
@@ -15,3 +14,7 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract(),
             }
+
+        next_page = response.css('li.next a::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
