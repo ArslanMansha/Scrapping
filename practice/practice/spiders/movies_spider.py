13b3aa2b968c0b0b12413c8bf35f567bbad01a17
ArslanMansha <arslan.mansha.arbisoft.com>
2018-08-31 14:45:44 +0500

    scrapping hdpopcorns.com a movie website

--- practice/practice/spiders/movies_spider.py
+++ practice/practice/spiders/movies_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-
+import re
 
 class MoviesSpider(scrapy.Spider):
     name = "movies"
@@ -16,12 +16,47 @@ class MoviesSpider(scrapy.Spider):
 
     def parse_movie_details(self, response):
         movie_details = response.xpath('/html/body/div[1]/div/article/div/div[1]/div[1]/div[1]/div[2]')
-        yield{
+        quality = movie_details.xpath('p[3]').re(r'>[0-9]+p<')
+        for iterator in range(len(quality)):
+            if len(quality[iterator]) > 5:
+                quality[iterator] = quality[iterator][1:-1]
+
+        language = movie_details.xpath('p[3]').re(r'Language[\s\w\W]*Genre')
+        language = re.findall(r'>[\w\s]+<', str(language))
+        for iterator in range(len(language)):
+            language[iterator] = language[iterator][1:-1]
+        # language.remove(' ')
+
+        genre = movie_details.xpath('p[3]').re(r'Genre[\s\w\W]*Cast')
+        genre = re.findall(r'>[\w\s]+<', str(genre))
+        for iterator in range(len(genre)):
+            genre[iterator] = genre[iterator][1:-1]
+        # genre.remove(' ')
+
+        cast = movie_details.xpath('p[3]').re(r'Cast[\s\w\W]*imdb/')
+        cast = re.findall(r'>[\w\s]+<', str(cast))
+        for iterator in range(len(cast)):
+            cast[iterator] = cast[iterator][1:-1]
+        # cast.remove(' ')
+
+        imdb_rating = movie_details.xpath('p[3]').re(r'strong>[\d\.]*</strong')
+        imdb_rating = re.findall(r'>[\d\.]*<', str(imdb_rating))
+        imdb_rating = imdb_rating[0][1:-1]
+
+        synopsis = movie_details.xpath('p[3]').re(r'Synopsis[\s\w\W]*</p>')
+        synopsis = re.findall(r'>[\w\W]*<', str(synopsis))
+        synopsis = synopsis[0][1:-1]
+
+        yield {
             'Title': movie_details.xpath('p[1]/text()').extract_first(),
-            'Year': movie_details.xpath('p[3]/a[1]/text()').extract(),
-            'Language': movie_details.xpath('p[3]/a[4]/text()').extract(),
-            'Download Link': response.urljoin(""),
-            'Thumb Nail': movie_details.xpath('p[2]/a/img/@src').extract(),
-            'Synopsis': movie_details.xpath('p[7]/text()').extract(),
+            'Year': movie_details.xpath('p[3]').re(r'>[0-9]{4}<')[0][1:5],
+            'Quality': quality,
+            'Language': language,
+            'Genre': genre,
+            'Cast': cast,
+            'IMDB rating': imdb_rating,
+            # 'Download Link': response.urljoin(""),
+            # 'Thumb Nail': movie_details.xpath('p[2]/a/img/@src').extract(),
+            'Synopsis': synopsis,
 
         }
